e9836ae876f2a3aba56dbac69519bbe921679de1
AryanBaghel-1 <albertmit98@gmail.com>
2026-09-13 03:44:18 +0530

    feat: add browser module for Prisma model exports

---
+++ packages/db/src/browser.ts
@@ -0,0 +1,8 @@
+export * from "../generated/prisma/enums";
+export type { UserModel as User } from "../generated/prisma/models/User";
+export type { OptionModel as Option } from "../generated/prisma/models/Option";
+export type { QuestionMetaModel as QuestionMeta } from "../generated/prisma/models/QuestionMeta";
+export type { TestModel as Test } from "../generated/prisma/models/Test";
+export type { TestQuestionModel as TestQuestion } from "../generated/prisma/models/TestQuestion";
+export type { TestAttemptModel as TestAttempt } from "../generated/prisma/models/TestAttempt";
+export type { AttemptAnswerModel as AttemptAnswer } from "../generated/prisma/models/AttemptAnswer";
